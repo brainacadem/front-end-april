3b1a64be3dcff87206c63283d8d52e7acbc36f64
TkachenkoArtem <igrfy87@gmail.com>
2016-05-31 21:45:21 +0300

    Finished values and primitives

--- Tkachenko/00_JS_base/02_values and primitives/script.js
+++ Tkachenko/00_JS_base/02_values and primitives/script.js
@@ -1,6 +1,6 @@
 // Task 1
 
-console.log('Task 1');
+console.log('__________Task 1__________');
 
 function task1(X, Y, Z, N) {
     console.log('You will be a ' + X + ' in ' + Y + ', and married to ' + Z + ' with ' + N + ' kids.');
@@ -10,14 +10,14 @@ task1('2200', 'Muckanaghederdauhaulia', 'Karen', '71');
 
 // Task 2
 
-console.log('Task 2');
+console.log('__________Task 2__________');
 
 function task2(currentMonth, currentYear, birthMonth, birthYear) {
     var age;
     if (currentMonth > birthMonth) {
-      age = currentYear - birthYear - 1;
+        age = currentYear - birthYear - 1;
     } else {
-      age =  currentYear - birthYear;
+        age = currentYear - birthYear;
     }
     console.log('You age ' + age + ' years.');
 
@@ -29,46 +29,55 @@ task2('7', '2016', '6', '1987');
 
 // Task 3
 
-console.log('Task 3');
+console.log('__________Task 3__________');
 
-function task3(age) {
+function task3(ageStart, ageFinish) {
 
-            console.log('Error');
+    var cake = 5 /*for 1 day*/ ;
+    var eat = (ageFinish - ageStart) * 365;
+    console.log('For ' + (ageFinish - ageStart) + ' years you eat ' + eat + ' cake!');
 }
 
-assignGrade('5');
+task3('35', '215');
 
 // Task 4
 
-console.log('Task 4');
+console.log('__________Task 4__________');
 
-function pluralize(noun, number) {
-    if ((number > 0) && (number != 1) && (noun != 'cats') && (noun != 'dogs')) {
-        console.log(+number + ' ' + noun + 's');
-    } else if ((number > 0) && (number == 1) && (noun != 'cats') && (noun != 'dogs')) {
-        console.log(+number + ' ' + noun);
-    } else if ((number > 0) && (number == 1) && (noun == 'cats')) {
-        console.log(+number + ' cat');
-    } else if ((number > 0) && (number == 1) && (noun == 'dogs')) {
-        console.log(+number + ' dog');
-    } else {
-        console.log('Error');
-    }
+function task4(radius) {
+
+    var circumference = radius * Math.PI * 2;
+    var area = Math.PI + Math.pow(radius, 2);
+
+    console.log('The circumference is ' + circumference);
+    console.log('The area is ' + area);
+
 }
-pluralize('cat', 5);
-pluralize('cats', 1);
-pluralize('dogs', 0);
-pluralize('dog', 666);
+
+task4('55');
 
 // Task 5
 
-console.log('Task 5');
+console.log('__________Task 5__________');
 
-function task5(a) {
-    var output = (a === 0) ? 'Верно!' : 'Неверно!';
-    console.log(output);
+function task5(value, direction) {
+
+    var result;
+
+    switch (direction) {
+        case 'CtoF':
+            result = (value * 1.8) + 32;
+            break;
+        case 'FtoC':
+            result = (value - 32)/1.8;
+            break;
+        default:
+            console.log('Error!!');
+    }
+
+    console.log(value + ' ' + direction + ' ' + result);
+
 }
 
-task5(1);
-task5(0);
-task5(-3);
+task5(2587, 'CtoF');
+task5(25647, 'FtoC');
