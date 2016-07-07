d51daf7cd12193febfa65e62629ea950cfe31801
TkachenkoArtem <igrfy87@gmail.com>
2016-07-07 11:06:04 +0300

    added haml

--- Tkachenko/05_MagBlog/gulpfile.js
+++ Tkachenko/05_MagBlog/gulpfile.js
@@ -12,6 +12,7 @@ var autoprefixer = require('gulp-autoprefixer');
 var uncss = require('gulp-uncss');
 var uglify = require('gulp-uglify');
 var livereload = require('gulp-livereload');
+var haml = require('gulp-haml');
 
 /*минимизация html*/
 gulp.task('html', () => {
@@ -22,6 +23,17 @@ gulp.task('html', () => {
         .pipe(rename('index.min.html'))
         .pipe(gulp.dest('dist'))
         .pipe(livereload());
+});
+
+/*создание haml*/
+gulp.task('haml', function () {
+  gulp.src('app/*.haml')
+    .pipe(haml())
+    .pipe(htmlmin({
+        collapseWhitespace: true
+    }))
+    .pipe(rename('index.min.html'))
+        .pipe(gulp.dest('dist'));
 });
 
 /*объединение сторонних css*/
@@ -49,7 +61,7 @@ gulp.task('png', () => {
 });
 
 /*создание css*/
-gulp.task('sass', function() {
+gulp.task('sass', () => {
     return gulp.src('app/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('app/css'));
@@ -78,15 +90,6 @@ gulp.task('css', () => {
         .pipe(livereload());
 });
 
-/*операции со шрифтами*/
-gulp.task('fonts', function() {
-    gulp.src(['fonts/*.ttf'])
-        .pipe(ttf2woff())
-        .pipe(ttf2woff2())
-        .pipe(ttf2eot())
-        .pipe(gulp.dest('dist/fonts/'));
-});
-
 /*операции с js*/
 gulp.task('compress', () => {
     return gulp.src('app/scripts/*.js')
@@ -97,33 +100,37 @@ gulp.task('compress', () => {
 
 /*операции со шрифтами*/
 gulp.task('copyfonts', function() {
-   gulp.src('app/fonts/*.{ttf,woff,woff2,eot,otf,svg,icon}')
-   .pipe(gulp.dest('dist/fonts'));
+    gulp.src('app/fonts/*.{ttf,woff,woff2,eot,otf,svg,icon}')
+        .pipe(gulp.dest('dist/fonts'));
 });
 
 /*инициализация*/
-gulp.task('init', function() {
-   gulp.src('bower_components/font-awesome/css/font-awesome.css')
-   .pipe(rename('font-awesome.vendor.css'))
-   .pipe(gulp.dest('app/css/'));
-   gulp.src('bower_components/normalize-css/normalize.css')
-   .pipe(rename('normalize.vendor.css'))
-   .pipe(gulp.dest('app/css/'));
+gulp.task('init', () => {
+    gulp.src('bower_components/font-awesome/css/font-awesome.css')
+        .pipe(rename('font-awesome.vendor.css'))
+        .pipe(gulp.dest('app/css/'));
+    gulp.src('bower_components/normalize-css/normalize.css')
+        .pipe(rename('normalize.vendor.css'))
+        .pipe(gulp.dest('app/css/'));
+    gulp.src('bower_components/font-awesome/fonts/*.{ttf,woff,woff2,eot,otf,svg}')
+        .pipe(gulp.dest('app/fonts'));
+    gulp.src('app/img/*.ico')
+        .pipe(gulp.dest('dist/img'));
 });
 
 /*WATCH*/
 gulp.task('watch', () => {
     livereload.listen();
     gulp.watch('app/*.html', ['html', 'vendorCSS']) /*следить за html, запускать html и vendorCSS*/
+    gulp.watch('app/*.haml', ['haml']) /*следить за haml, запускать haml*/
     gulp.watch('app/css/*.min.css', ['vendorCSS']) /*следить за сторонними css, запускать vendorCSS*/
     gulp.watch('app/img/*.png', ['png']) /*следить за изображениями, запускать png*/
     gulp.watch('app/scss/**/*.scss', ['sass']) /*следить за SASS, запускать sass*/
     gulp.watch('app/css/style.css', ['css']) /*следить за style.css, запускать css*/
     gulp.watch('app/scripts/*.js', ['compress']) /*следить за js, запускать compress*/
-
 });
 
 /*BUILD: step1: gulp html sass css compress png  step2: gulp vendorCSS*/
-gulp.task('build', ['html', 'png', 'sass', 'css', 'compress', 'vendorCSS', 'copyfonts'], function() {
+gulp.task('build', ['haml', 'html', 'png', 'sass', 'css', 'compress', 'vendorCSS', 'copyfonts'], function() {
     console.log('Building completed!');
 })
